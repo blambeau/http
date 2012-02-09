901290de28121ebae8e89976ede69c0ab56e0f01
Bernard Lambeau <blambeau@gmail.com>
2012-02-09 15:50:48 +0100

    Don't spread the :response option secret.
    The handling of :response option is spread in two or three different
    places so far. It might make sense to regroup it in a unique place.
    This commit frees Chainable#head from that management.

--- lib/http/chainable.rb
+++ lib/http/chainable.rb
@@ -2,7 +2,7 @@ module Http
   module Chainable
     # Request a get sans response body
     def head(uri, options = {})
-      request :head, uri, {:response => :object}.merge(options)
+      request :head, uri, options
     end
 
     # Get a resource
@@ -47,7 +47,7 @@ module Http
 
     # Make an HTTP request with the given verb
     def request(verb, uri, options = {})
-      options[:response] ||= :parsed_body
+      options[:response] ||= (verb == :head ? :object : :parsed_body)
 
       if options[:headers]
         headers = default_headers.merge options[:headers]
